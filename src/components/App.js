d20598d5c68f2649fd0d9190df5f6feab624f178
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2022-06-18 17:13:50 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 const App = () => {
+  const [name, setName] = useState("Marco");
+  const [btn, setBtn] = useState("Polo");
+
+  function changeHandle() {
+    if (name === "Marco") {
+      setName("Polo");
+      setBtn("Marco");
+    } else {
+      setName("Marco");
+      setBtn("Polo");
+    }
+  }
+
   return (
     <div id="main">
-      <h1>Rohit ghg</h1>
+      <h1 id="marco-polo">{name}</h1>
+      <button id="marco-polo-toggler" onClick={changeHandle}>
+        {btn}
+      </button>
     </div>
   );
 };
