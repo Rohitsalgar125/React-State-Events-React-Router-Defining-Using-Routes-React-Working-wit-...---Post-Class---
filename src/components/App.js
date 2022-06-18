85ff35dc42a36e321488e4af2ae90db08ed676d4
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2022-06-18 17:06:30 +0530

    hi

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
-  
   return (
     <div id="main">
-
+      <h1>Rohit ghg</h1>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
